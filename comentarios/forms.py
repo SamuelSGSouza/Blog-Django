75b2e5d2ab0d9d3f18e83a119568d19d2c8cc060
samuel <samuels.g.desouza@gmail.com>
2021-10-18 18:57:45 -0300

    Inserindo verificação reCAPTCHA no envio de comentários

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,8 +1,33 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 class FormComentario(ModelForm):
     def clean(self):
+        raw_data = self.data
+        recaptch_response = raw_data.get('g-recaptcha-response')
+        recaptch_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6LfCFtwcAAAAAB3qJQRsRhnTjdakSv0Z9PkVTrW0',
+                'response': recaptch_response
+            }
+        )
+
+        recaptch_result = recaptch_request.json()
+        if not recaptch_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpe, MR. Robot.'
+            )
+
+        #https://www.google.com/recaptcha/api/siteverify
+        #secret
+        #response
+
+
+
+        print(raw_data)
         data = self.cleaned_data
         nome = data.get('nome_comentario')
         email = data.get('email_comentario')
--- projeto/settings.py
+++ projeto/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lwy528%jhy*uuz0io^r-8x&tkedejezeueih)xvl-5zo^k0k4_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
